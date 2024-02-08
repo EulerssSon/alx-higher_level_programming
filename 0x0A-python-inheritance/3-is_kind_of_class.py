e92dffddfc3be27193e3056e671e1876bbf92674
EulerssSon <alfy.kerolous@gmail.com>
2024-02-07 16:24:02 -0800

    Fiox

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,7 +1,7 @@
-#!/usr/bin/pyhon3
-"""Module for is_kind_of_class method."""
+#!/usr/bin/python3
+'''Module for is_kind_of_class method.'''
 
 
 def is_kind_of_class(obj, a_class):
-    """Method to check ifisinstance of, or if the object is an"""
+    '''Determines if an object is a subclass of a class.'''
     return isinstance(obj, a_class)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,21 +1,7 @@
-FG
-"""Module to check if an object is an instance of a class that inherited
-directly or indirectly from the specified class"""
+#!/usr/bin/python3
+'''Module for inherits_from method.'''
 
 
 def inherits_from(obj, a_class):
-    """Check if an object is an instance of a class that inherited
-    directly or indirectly from the specified class
-
-    Args:
-        obj: object to check
-        a_class: class to check
-
-    Returns:
-        True if the object is an instance of a class that inherited
-        directly or indirectly from the specified class; otherwise False
-    """
-    if isinstance(obj, a_class) and type(obj) != a_class:
-        return True
-    else:
-        return False
+    '''Determines if an object is a true subclass of a class.'''
+    return isinstance(obj, a_class) and type(obj) != a_class
