0d2c3771fae4dbe5e0cc99be3ed1864ef894843c
EulerssSon <alfy.kerolous@gmail.com>
2024-02-16 02:43:30 -0800

    Safe print Int

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+from sys import stderr
+def safe_print_integer_err(value):
+    is_valid = False
+    try:
+        print("{:d}".format(value))
+        is_valid = True
+    except Exception as err:
+        print("Exception:", err, file=stderr)
+    finally:
+        return is_valid
