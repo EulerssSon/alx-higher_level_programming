aeead87c1b7285ed8f7802045d267fc829d91d5f
EulerssSon <alfy.kerolous@gmail.com>
2024-02-16 01:26:00 -0800

    Dfasd

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+    int_count = 0
+
+    while (x > 0):
+        try:
+            print("{:d}".format(my_list[counter]), end="")
+            int_count += 1
+        except (IndexError) as err:
+            raise err
+        except (ValueError, TypeError):
+            pass
+        finally:
+            counter += 1
+            x -= 1
+    print()
+    return int_count
