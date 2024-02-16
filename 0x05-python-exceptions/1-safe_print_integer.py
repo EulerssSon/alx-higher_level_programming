7f07f74f68b81618204e7e564f3d17ab314de625
EulerssSon <alfy.kerolous@gmail.com>
2024-02-16 01:12:28 -0800

    a

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        print("{:d}".form)
-    except Exception as e:
+        print("{:d}".format(value))
+    except (TypeError, ValueError):
         return False
     else:
         return True
