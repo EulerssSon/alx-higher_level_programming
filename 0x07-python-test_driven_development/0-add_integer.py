dc96d31705fdb7e96577eb3ed550c41ded2582fb
EulerssSon <alfy.kerolous@gmail.com>
2024-02-26 02:06:59 -0800

    chechingfor a = null

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,9 +14,9 @@ def add_integer(a, b=98) -> int:
     Raises:
         TypeError: If a or b is not an integer
     """
-    if type(a) not in [int, float]:
+    if type(a) not in [int, float] or a is None:
         raise TypeError('a must be an integer')
-    if type(b) not in [int, float]:
+    if type(b) not in [int, float] or b is None:
         raise TypeError('b must be an integer')
     return int(a) + int(b)
 
