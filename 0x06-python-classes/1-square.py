477f17ac013d783d717750b236ecdb2c23f68855
EulerssSon <alfy.kerolous@gmail.com>
2024-02-25 18:12:43 -0800

    added1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Module for Square class"""
+
+
+class Square:
+    """A square class"""
+    def __init__(self, size: int) -> None:
+        """Init method
+
+        Args:
+            size (int): Size of the square
+        """
+        self.__size = size
