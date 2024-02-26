8b1406c387be641d781440bb87ee4b2ccf3b7259
EulerssSon <alfy.kerolous@gmail.com>
2024-02-26 03:25:04 -0800

    taks 4 and 5 done

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""This module prints a square with the character '#'"""
+
+
+def print_square(size):
+    """Prints a square with the character '#'
+
+    Args:
+        size (int): the size of the square
+
+    Returns:
+        None
+
+    Raises:
+        TypeError: If size is not an integer
+        ValueError: If size is less than 0
+    """
+    if type(size) is not int:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    for i in range(size):
+        print('#' * size)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""This Moudle is to print /n/n after .?:"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after each of these characters: ., ? and:
+
+    Args:
+        text (str): the text to print
+
+    Returns:
+        None
+
+    Raises:
+        TypeError: If text is not a string
+    """
+    if type(text) is not str or text is None:
+        raise TypeError('text must be a string')
+    for char in '.:?':
+        text = (char + '\n\n').join(
+            [line.strip(' ') for line in text.split(char)])
+    print(text, end='')
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
