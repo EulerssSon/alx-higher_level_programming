24852c305ab5e4e27f716f8214a0b67b7fd7b16e
EulerssSon <alfy.kerolous@gmail.com>
2024-02-26 23:25:52 -0800

    a

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -7,6 +7,7 @@ class Square(Rectangle):
     '''A subclass representing a rectangle.'''
     def __init__(self, size):
         '''Constructor.'''
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
