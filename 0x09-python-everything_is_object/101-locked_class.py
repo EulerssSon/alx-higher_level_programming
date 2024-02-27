df84ecdd0aca56b98e0debdaf7c442ce557dc6f1
EulerssSon <alfy.kerolous@gmail.com>
2024-02-26 21:52:06 -0800

    done

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""This module contains a class that is locked down with __slots__"""
+
+
+class LockedClass:
+    """Prevent from adding new attributes to the class.
+    except for the first_name attribute
+    """
+    __slots__ = ("first_name")
