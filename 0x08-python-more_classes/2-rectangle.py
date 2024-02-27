2523088ee09e84533a4fa13409f6e8495e88ece4
EulerssSon <alfy.kerolous@gmail.com>
2024-02-26 17:24:21 -0800

    added two features are preminter

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,1 +1,83 @@
 #!/usr/bin/python3
+"""This module To Represent a Rectangle"""
+
+
+class Rectangle:
+    """Class Rectangle"""
+    def __init__(self, width=0, height=0) -> None:
+        """Constructor for rect with width and height
+
+        Args:
+            width (int, optional): Rect width. Defaults to 0.
+            height (int, optional): Rect height. Defaults to 0.
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self) -> int:
+        """Getter for width
+
+        Returns:
+            int: Width of rect
+        """
+        return self.__width
+
+    @property
+    def height(self) -> int:
+        """Getter for height
+
+        Returns:
+            int: Height of rect
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value: int) -> None:
+        """Setter for width
+
+        Args:
+            value (int): Width of rect
+
+        Raises:
+            TypeError: If width is not an int
+            ValueError: If width is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @height.setter
+    def height(self, value: int) -> None:
+        """Setter for height
+
+        Args:
+            value (int): Height of rect
+
+        Raises:
+            TypeError: If height is not an int
+            ValueError: If height is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self) -> int:
+        """Calculate area of rect
+
+        Returns:
+            int: Area of rect
+        """
+        return self.width * self.height
+
+    def perimeter(self) -> int:
+        """Calculate perimeter of rect
+
+        Returns:
+            int: Perimeter of rect
+        """
+        return 2 * (self.width + self.height)
