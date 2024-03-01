9d2ccb4cc1236fe09edcde770c37161ab5029f08
EulerssSon <alfy.kerolous@gmail.com>
2024-03-01 12:54:45 -0800

    first one

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""This the package. It contains the base class for the base class."""
+
+
+class Base:
+    """This is the base class for the base class."""
+    __no_objects = 0
+
+    def __init__(self, id=None):
+        """construct object
+
+        Args:
+            id (int, optional): id of the object
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__no_objects += 1
+            self.id = Base.__no_objects
