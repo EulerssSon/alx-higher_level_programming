e02a740d5d11b47cbf75182e0c14045dd0677287
EulerssSon <alfy.kerolous@gmail.com>
2024-03-01 13:56:14 -0800

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""This module is for the square one inherts from rect"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """sq Class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Construct object
+
+        Args:
+            size (int): size of square
+            x (int, optional): x center
+            y (int, optional): y center
+            id (int, optional): id of the object
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self) -> str:
+        """return string representation"""
+        i = self.id
+        x = self.x
+        y = self.y
+        w = self.width
+        return f"[Square] ({i}) {x}/{y} - {w}"
