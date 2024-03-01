1c790cc8b9aa8c57a1a555b7cc45716ee353c9bf
EulerssSon <alfy.kerolous@gmail.com>
2024-03-01 14:54:21 -0800

    to json to class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -32,3 +32,19 @@ class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save list of objects to file
+
+        Args:
+            list_objs (list): list of objects
+        """
+        file_name = f"{cls.__name__}.json"
+        list_dicts = []
+        with open(file_name, mode="w") as file:
+            if list_objs is not None:
+                for obj in list_objs:
+                    list_dicts.append(obj.to_dictionary())
+            file_str = cls.to_json_string(list_dicts)
+            file.write(file_str)
