69cc83b5d76c104c4ef6928ca2a2d66afc839f5d
EulerssSon <alfy.kerolous@gmail.com>
2024-03-01 13:43:18 -0800

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -132,14 +132,18 @@ class Rectangle(Base):
         return f"[Rectangle] ({i}) {x}/{y} - {w}/{h}"
 
     def update(self, *args: tuple) -> None:
-        """update rect"""
-    if len(args) > 0:
-        self.id = args[0]
-    if len(args) > 1:
-        self.width = args[1]
-    if len(args) > 2:
-        self.height = args[2]
-    if len(args) > 3:
-        self.x = args[3]
-    if len(args) > 4:
-        self.y = args[4]
+        """This method updates the attributes of the object
+
+        Args:
+            args (tuple): tuple of args to update
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
