5ad21ba4853a4dbc19031cdc9e90d447fca5fd30
EulerssSon <alfy.kerolous@gmail.com>
2024-03-12 19:54:54 -0700

    del

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- script script is to update Bob score to 10
+UPDATE `second_table` SET `score` = 10 WHERE `name` = "Bob";
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,2 @@
+-- script script is to delete all records from the table where scr is <= 5
+DELETE FROM `second_table` WHERE `score` <= 5;
