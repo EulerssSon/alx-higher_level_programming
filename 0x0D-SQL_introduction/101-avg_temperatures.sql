b335f241c3b19174acdc953b8c455260418c3d4e
EulerssSon <alfy.kerolous@gmail.com>
2024-03-14 15:38:49 -0700

    order desc

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- this script is to display the avg temperature of each city in the world desc
-SELECT `city`, AVG(`temperature`) AS `avg_temperature`
+SELECT `city`, AVG(`value`) AS `avf_temperature`
 FROM `temperature`
 GROUP BY `city`
 ORDER BY `avg_temperature` DESC;
