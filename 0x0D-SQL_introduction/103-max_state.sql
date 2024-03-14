7f7a763c397973304269174f792b23c30ec41d84
EulerssSon <alfy.kerolous@gmail.com>
2024-03-14 16:05:16 -0700

    a

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,5 +1,5 @@
--- this script is to display the avg temperature of each city in the world desc
-SELECT `state`, MAX(`value`) AS `avg_temp`
+-- Displays the max temperature of each state, ordered by state name.
+SELECT `state`, MAX(`value`) AS `max_temp`
 FROM `temperatures`
 GROUP BY `state`
 ORDER BY `state`;
