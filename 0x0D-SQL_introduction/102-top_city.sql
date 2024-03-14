35d3cd5ab0294400a0076c6e71cb6e1ce6264903
EulerssSon <alfy.kerolous@gmail.com>
2024-03-14 15:56:33 -0700

    top

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,6 @@
+-- this script is to display the avg temperature of each city in the world desc
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+WHERE `month` = 7 OR `month` = 8
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
