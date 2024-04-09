e075cbdd5f4ae541baf5e6a15628ab6b48bf66d0
EulerssSon <alfy.kerolous@gmail.com>
2024-04-08 19:39:50 -0700

    push

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,6 @@ if (argv.length <= 3) {
   console.log('0');
 } else {
   const numbers = argv.slice(2).map(Number);
-  numbers.sort();
-  const uniqueNumbers = [...new Set(numbers)];
-  console.log(uniqueNumbers[uniqueNumbers.length - 2]);
+  const second = numbers.sort(function(a, b) { return b - a; })[1];
+  console.log(second);
 }
