a59bd64289e33b6012f7d193132153e5ae4959b3
EulerssSon <alfy.kerolous@gmail.com>
2024-04-08 17:58:27 -0700

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+function fact (factorial) {
+  if (isNaN(factorial)) {
+    return 1;
+  }
+  for (let i = factorial - 1; i >= 1; i--) {
+    factorial *= i;
+  }
+  return factorial;
+}
+console.log(fact(parseInt(argv[2])));
