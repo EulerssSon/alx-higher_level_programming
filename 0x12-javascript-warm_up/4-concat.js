d30144cfd31dd74ee280c1a78c6a54d6a5284226
EulerssSon <alfy.kerolous@gmail.com>
2024-04-08 17:21:32 -0700

    done

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+let firstArg;
+let secondArg;
+if (argv.length > 2) {
+  firstArg = argv[2];
+}
+if (argv.length > 3) {
+  secondArg = argv[3];
+}
+console.log(`${firstArg} is ${secondArg}`);
