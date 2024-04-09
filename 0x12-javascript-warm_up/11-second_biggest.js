497a7d8a44916b5fe69e891529ad9ab40b79dccc
EulerssSon <alfy.kerolous@gmail.com>
2024-04-08 19:43:58 -0700

    is it done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 if (argv.length <= 3) {
-  console.log('0');
+  console.log(0);
 } else {
   const numbers = argv.slice(2).map(Number);
-  const second = numbers.sort(function(a, b) { return b - a; })[1];
-  console.log(second);
+  numbers.sort();
+  uniqueNumbers = [...new Set(numbers)];
+  console.log(uniqueNumbers[uniqueNumbers.length - 2]);
 }
