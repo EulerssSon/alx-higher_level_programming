a1098d83a9e2e690e398cc34c3075b59d6d8d6eb
EulerssSon <alfy.kerolous@gmail.com>
2024-04-08 17:46:03 -0700

    PrintSquare

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+if (argv.length < 3 || isNaN(argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < parseInt(argv[2]); i++) {
+    let character = 'X';
+    for (let j = 0; j < parseInt(argv[2]) - 1; j++) {
+      character += 'X';
+    }
+    console.log(character);
+  }
+}
