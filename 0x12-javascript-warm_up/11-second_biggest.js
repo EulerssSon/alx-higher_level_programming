f5facd4a8a9049299c32ea8f527f99a73c98dbe5
EulerssSon <alfy.kerolous@gmail.com>
2024-04-08 18:16:34 -0700

    god help

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
 if (argv.length <= 3) {
-  console.log(0);
+  console.log('0');
 } else {
   const numbers = argv.slice(2).map(Number);
   numbers.sort();
