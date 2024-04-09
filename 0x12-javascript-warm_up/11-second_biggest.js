2a0b84846bfca09cf9e077c763e803cc1edc7691
EulerssSon <alfy.kerolous@gmail.com>
2024-04-08 18:07:48 -0700

    second largest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+if (argv.length <= 3) {
+  console.log(0);
+} else {
+  argv.shift();
+  argv.shift();
+  argv.sort();
+  console.log(argv[argv.length - 2]);
+}
