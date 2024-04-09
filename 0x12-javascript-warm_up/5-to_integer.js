51975495f1dd8b868e6f5bd5ec9dba5186c0415c
EulerssSon <alfy.kerolous@gmail.com>
2024-04-08 17:27:01 -0700

    Parse Int

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+const { argv } = require('node:process');
+let firstArg;
+if (argv.length <= 2) {
+  console.log('Not a number');
+} else if (argv.length > 2) {
+  firstArg = argv[2];
+  if (isNaN(firstArg)) {
+    console.log('Not a number');
+  } else {
+    intFirstArg = parseInt(firstArg);
+    console.log(`My number: ${intFirstArg}`);
+  }
+}
