5325ccfeb3c8f62dfcfcf8114308bb4dffee454e
EulerssSon <alfy.kerolous@gmail.com>
2024-04-09 19:38:46 -0700

    i hope so

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,11 +5,15 @@ class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
+
   charPrint (c) {
-    if (c) {
-      Rectangle.char = c;
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
-    this.print();
   }
 }
 
