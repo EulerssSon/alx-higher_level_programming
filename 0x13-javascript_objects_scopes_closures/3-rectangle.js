b24e6ef6e51c8fdc44f8b25191f7b57f86cd7b1a
EulerssSon <alfy.kerolous@gmail.com>
2024-04-09 19:04:25 -0700

    3 done

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+class Rectangle {
+  static char = 'X';
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log(Rectangle.char.repeat(this.width));
+    }
+  }
+}
+module.exports = Rectangle;
