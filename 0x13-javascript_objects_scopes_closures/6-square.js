e97385d5ab8f6b01a527476069ceb077e65c8580
EulerssSon <alfy.kerolous@gmail.com>
2024-04-09 19:31:11 -0700

    change char

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+  charPrint (c) {
+    if (c) {
+      Rectangle.char = c;
+    }
+    this.print();
+  }
+}
+
+module.exports = Square;
