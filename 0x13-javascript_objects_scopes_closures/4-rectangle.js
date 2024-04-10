0fe09d3a2e6c1a4fafe5fbe5635b98d838603d79
EulerssSon <alfy.kerolous@gmail.com>
2024-04-09 19:10:23 -0700

    rotaate and double now

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,28 @@
 #!/usr/bin/node
+class Rectangle {
+  static char = 'X';
+
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log(Rectangle.char.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
