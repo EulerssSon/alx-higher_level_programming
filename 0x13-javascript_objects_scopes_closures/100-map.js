039a36c0864c2ca2eba04cac3ba9cb55a3fe9b8c
EulerssSon <alfy.kerolous@gmail.com>
2024-04-12 09:13:12 -0700

    map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const my_list = require('./100-data.js').list;
 const new_list = my_list.map(Number);
-for (let i = 0; i < new_list.length; ++i) {
-  new_list[i] *= i
-}
+cost new_list = my_list.map((num) => (num * 2));
 console.log(my_list);
 console.log(new_list);
