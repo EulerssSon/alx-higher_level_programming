f88013de59018d02acceaaa68cdfd989a7d21b6b
EulerssSon <alfy.kerolous@gmail.com>
2024-04-12 10:15:47 -0700

    doen

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+const fs = require('fs');
+const [,, file1, file2, outputFile] = process.argv;
+
+function concatenateFiles (file1Path, file2Path, outputPath) {
+  fs.readFile(file1Path, 'utf8', (err, data1) => {
+    if (err) {
+      return;
+    }
+    fs.readFile(file2Path, 'utf8', (err, data2) => {
+      if (err) {
+        return;
+      }
+      const combinedContent = data1 + data2;
+      fs.writeFile(outputPath, combinedContent, (err) => {
+        if (err) {
+          return;
+        }
+      });
+    });
+  });
+}
+concatenateFiles(file1, file2, outputFile);
