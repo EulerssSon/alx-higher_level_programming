17c236d1f0d5492c7052cf04e86e3e9f8674fdc6
EulerssSon <alfy.kerolous@gmail.com>
2024-04-12 09:32:27 -0700

    heh

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
-const newList = list.map(n => n * (list.indexOf(n)));
 console.log(list);
-console.log(newList);
+console.log(list.map((x, idx) => x * idx));
